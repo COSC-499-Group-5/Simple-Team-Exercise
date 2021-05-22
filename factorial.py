9a76b567b90850ced9430d6daa37f286610c64d5
Guy <gkamin6@gmail.com>
2021-05-22 00:20:01 -0700

    Add hashmap to store already seen values

--- factorial.py
+++ factorial.py
@@ -1,13 +1,21 @@
 def factorial(number):
+    # initilize hash map
+    dpArray = {}
+    # if number is in hash map, return the factorial version of that number
+    if number in dpArray:
+        return dpArray[number]
+    
     # Cannot take factorial of negative number
     if number <= -1:
         return "cannot take factorial of a negative number"
     
     # base case 0! = 1
-    if number == 0:
+    if number == 0 or number == 1:
         return 1
     
     # else call recursive 
-    return number * factorial(number - 1)
+    fact = number * factorial(number - 1)
+    dpArray[number] = fact
+    return fact
 
-    
+print(factorial(5))
